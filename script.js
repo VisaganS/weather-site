67696e90b60b21fc473ae601ead2efa75d3ab160
Visagan Shan <visagan.shan@gmail.com>
2023-05-12 16:51:33 -0400

    edited 5day schedule

--- script.js
+++ script.js
@@ -51,10 +51,50 @@ geoData.then(result => {
     console.error(error);
   });
   
-  const fiveDayData = axios.get(`https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}${apiKey}`);
+  const fiveDayData = axios.get(`https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}${apiKey}${units}`);
   fiveDayData.then(result =>{
-    console.log(result.data);
-  })
+    
+    console.log(result.data)
+
+
+    //get each day's temp
+    arrTemp= [result.data.list[0].main.temp,result.data.list[5].main.temp,result.data.list[10].main.temp,result.data.list[17].main.temp,
+    result.data.list[25].main.temp];
+    console.log(arrTemp);
+
+    //get each day's image code
+    arrImg = [result.data.list[0].weather[0].icon,result.data.list[5].weather[0].icon,result.data.list[10].weather[0].icon,
+    result.data.list[17].weather[0].icon,result.data.list[25].weather[0].icon];
+    console.log(arrImg);
+
+    const weekEl = document.querySelector(".week");
+
+    for(let i = 0; i < arrTemp.length; i++){
+    let weekDayEl = document.createElement("div");
+    weekDayEl.classList.add("week-day");
+
+    let weekDayHeadEl = document.createElement("div");
+    weekDayHeadEl.classList.add("week-day__head");
+    weekDayHeadEl.innerText = "Mon";
+    weekDayEl.appendChild(weekDayHeadEl);
+    
+
+    let weekDayIconEl = document.createElement("img");
+    weekDayIconEl.classList.add("week-day__icon");
+    weekDayIconEl.src = `https://openweathermap.org/img/wn/${arrImg[i]}@2x.png`;
+    weekDayEl.appendChild(weekDayIconEl);
+
+    let weekDayTempEl = document.createElement("div");
+    weekDayTempEl.classList.add("week-day__temp");
+    weekDayTempEl.innerText = `${Math.trunc(arrTemp[i])} `;
+    weekDayEl.appendChild(weekDayTempEl);
+
+    let weekDay
+    weekEl.appendChild(weekDayEl);
+    }
+  }).catch(error => {
+    console.error(error);
+  });
 
 }).catch(error => {
   console.error(error);
