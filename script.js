7b52307d72e2bf409ebfaef7419ea8abd99cbb97
Visagan Shan <visagan.shan@gmail.com>
2023-05-12 16:58:20 -0400

    completed code

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 const apiKey = '&appid=28e9edd8d1cede1c14b31dea008f36f7';
 const units = '&units=metric';
-let city = "Mississauga,";
-let state = 'ON,';
-let countryCode = 'CA';
+let city;
+let state;
+let countryCode;
 let lat = 40.7128;
 let lon = 74.0060;
 let temp;
@@ -52,10 +52,65 @@ function makeRequest(lat, lon) {
       console.error(error);
     });
     
-    const fiveDayData = axios.get(`https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}${apiKey}`);
+    const fiveDayData = axios.get(`https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}${apiKey}${units}`);
     fiveDayData.then(result =>{
-      console.log(result.data);
-    })
+      
+      console.log(result.data)
+  
+  
+      //get each day's temp
+      arrTemp= [result.data.list[0].main.temp,result.data.list[5].main.temp,result.data.list[10].main.temp,result.data.list[17].main.temp,
+      result.data.list[25].main.temp];
+      console.log(arrTemp);
+  
+      //get each day's image code
+      arrImg = [result.data.list[0].weather[0].icon,result.data.list[5].weather[0].icon,result.data.list[10].weather[0].icon,
+      result.data.list[17].weather[0].icon,result.data.list[25].weather[0].icon];
+      console.log(arrImg);
+  
+      const weekEl = document.querySelector(".week");
+      weekEl.innerHTML = "";
+  
+      for(let i = 0; i < arrTemp.length; i++){
+      let weekDayEl = document.createElement("div");
+      weekDayEl.classList.add("week-day");
+  
+      let weekDayHeadEl = document.createElement("div");
+      weekDayHeadEl.classList.add("week-day__head");
+        if(i === 0){
+          weekDayHeadEl.innerText = "Sat";
+        }else if(i == 1){
+         weekDayHeadEl.innerText = "Sun";
+        }else if(i == 2){
+          weekDayHeadEl.innerText = "Mon";
+        }else if(i == 3){
+          weekDayHeadEl.innerText = "Tue";
+        }else{
+          weekDayHeadEl.innerText = "Wed";
+        }
+
+
+
+
+      weekDayEl.appendChild(weekDayHeadEl);
+      
+  
+      let weekDayIconEl = document.createElement("img");
+      weekDayIconEl.classList.add("week-day__icon");
+      weekDayIconEl.src = `https://openweathermap.org/img/wn/${arrImg[i]}@2x.png`;
+      weekDayEl.appendChild(weekDayIconEl);
+  
+      let weekDayTempEl = document.createElement("div");
+      weekDayTempEl.classList.add("week-day__temp");
+      weekDayTempEl.innerText = `${Math.trunc(arrTemp[i])} `;
+      weekDayEl.appendChild(weekDayTempEl);
+  
+      let weekDay
+      weekEl.appendChild(weekDayEl);
+      }
+    }).catch(error => {
+      console.error(error);
+    });
   
  
 }
